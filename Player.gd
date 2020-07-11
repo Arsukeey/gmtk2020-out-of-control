e01661a4ddc41fd462388d076b2f286076a5805e
Alice Micheloni <alicemicheloni@tutanota.com>
2020-07-10 21:08:37 -0300

    aaa

--- Player.gd
+++ Player.gd
@@ -4,6 +4,21 @@ signal turn
 
 var can_move = true
 
+var spell0 = null
+var spell1 = null
+var spell2 = null
+
+func generate_random_attacks(): 
+    var attacks = get_tree().get_nodes_in_group("attack")
+    attacks.shuffle()
+    spell0 = attacks[0]
+    spell1 = attacks[1]
+    spell2 = attacks[2]
+
+func _ready():
+    generate_random_attacks()
+    pass
+
 func _process(_delta):
     var sqr_size = get_parent().SQR_SIZE
     if can_move:
@@ -23,6 +38,14 @@ func _process(_delta):
             emit_signal("turn")
             can_move = false
             $TimerMoveDelay.start()
+        draw_spell_on_menu()
+
+func draw_spell_on_menu():
+    spell1.visible = true
+    spell1.get_node("Sprite").visible = false
+    var spell1_node = spell1.get_node("Node2D")
+    spell1_node.visible = true
+    spell1_node.position = $CanvasLayer/SpellMenu/Spell1Menu.position
 
 func _on_TimerSkip_timeout():
     emit_signal("turn")
--- World.gd
+++ World.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+export (PackedScene) var Enemy
 
 # Out of Control
 
@@ -14,13 +15,25 @@ extends Node2D
 # and see what happens when you're no longer in charge.
 
 const SQR_SIZE = 32
-export var width = 30
-export var height = 20
+var width = 30
+var height = 20
 
+enum Things {
+    Player,
+    Enemy,    
+}
+var grid = []
 #var floor_image = preload("res://assets/gridsquare.png")
 
 func _ready():
     randomize()
+    $Player.position = Vector2((randi() % width) * SQR_SIZE, (randi() % height) * SQR_SIZE)
+    
+    for i in range(3):
+        var enemy1 = Enemy.instance()
+        enemy1.position = Vector2((randi() % width) * SQR_SIZE, (randi() % height) * SQR_SIZE)
+        add_child(enemy1)
+        
 #    for i in range(width):
 #        for j in range(height):
 #            var instance_floor = Position2D.new()
@@ -30,11 +43,12 @@ func _ready():
 #            floor_sprite.texture = floor_image
 #            instance_floor.add_child(floor_sprite)
 #            add_child(instance_floor)
-
+    
 func _process(delta):
     $TimerLbl.text = "%.1f" % $TimerSkip.time_left
-    print($TimerSkip.time_left)
 
 func _on_Player_turn():
+    grid[$Player.position.x / SQR_SIZE][$Player.position.y / SQR_SIZE] = "player"
+    print(grid)
     $TimerSkip.wait_time = randf() * 10.0 + 0.4
     $TimerSkip.start()
